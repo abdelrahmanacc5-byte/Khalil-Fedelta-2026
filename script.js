837babbae88a81ebb22eca69803e2f2c550d5213
abdelrahmanacc5-byte <abdelrahmanacc5@gmail.com>
2026-06-26 22:09:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
 let cart = [];
 let cartCount = 0;
+let total = 0;
 
-function addToCart(name, price, stockId) {
+const cartBtn = document.getElementById("cartBtn");
+const cartSidebar = document.getElementById("cartSidebar");
+const closeCart = document.getElementById("closeCart");
+const cartItems = document.getElementById("cartItems");
+const totalPrice = document.getElementById("totalPrice");
 
-let stock = document.getElementById(stockId);
+cartBtn.onclick = function () {
+    cartSidebar.classList.add("active");
+}
 
-let quantity = parseInt(stock.innerText);
+closeCart.onclick = function () {
+    cartSidebar.classList.remove("active");
+}
 
-if(quantity <= 0){
+function addToCart(name, price, stockId){
+
+let stock=document.getElementById(stockId);
+
+let quantity=parseInt(stock.innerText);
+
+if(quantity<=0){
 
 alert("Prodotto esaurito!");
+
 return;
 
 }
 
 quantity--;
 
-stock.innerText = quantity;
+stock.innerText=quantity;
 
 cart.push({
 name:name,
@@ -25,7 +41,66 @@ price:price
 
 cartCount++;
 
-document.getElementById("cartCount").innerText = cartCount;
+document.getElementById("cartCount").innerText=cartCount;
 
-alert(name + " aggiunto al carrello!");
+updateCart();
+
+}
+
+function updateCart(){
+
+cartItems.innerHTML="";
+
+total=0;
+
+cart.forEach((item,index)=>{
+
+total+=item.price;
+
+cartItems.innerHTML+=`
+
+<div class="cart-item">
+
+<div>
+
+<h4>${item.name}</h4>
+
+<p>€${item.price}</p>
+
+</div>
+
+<button onclick="removeItem(${index})">
+
+<i class="fa-solid fa-trash"></i>
+
+</button>
+
+</div>
+
+`;
+
+});
+
+if(cart.length==0){
+
+cartItems.innerHTML="<p>Il carrello è vuoto.</p>";
+
+}
+
+totalPrice.innerText=total.toFixed(2);
+
+}
+
+function removeItem(index){
+
+cart.splice(index,1);
+
+cartCount--;
+
+if(cartCount<0) cartCount=0;
+
+document.getElementById("cartCount").innerText=cartCount;
+
+updateCart();
+
 }
