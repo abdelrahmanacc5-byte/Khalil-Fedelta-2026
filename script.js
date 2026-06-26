6d581948daff07487c959587d532df7792a44918
abdelrahmanacc5-byte <abdelrahmanacc5@gmail.com>
2026-06-26 22:04:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+let cart = [];
+let cartCount = 0;
 
+function addToCart(name, price, stockId) {
+
+let stock = document.getElementById(stockId);
+
+let quantity = parseInt(stock.innerText);
+
+if(quantity <= 0){
+
+alert("Prodotto esaurito!");
+return;
+
+}
+
+quantity--;
+
+stock.innerText = quantity;
+
+cart.push({
+name:name,
+price:price
+});
+
+cartCount++;
+
+document.getElementById("cartCount").innerText = cartCount;
+
+alert(name + " aggiunto al carrello!");
+}
